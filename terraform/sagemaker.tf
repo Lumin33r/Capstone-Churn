8058d6a47684cfed7325a4f5b0d31f32fc0cac79
Lumineer <troy.arris@gmail.com>
2026-04-07 21:11:12 -0400

    terraform.yml and *.tf changes

--- terraform/sagemaker.tf
+++ terraform/sagemaker.tf
@@ -1,48 +1,23 @@
-resource "aws_ecr_repository_policy" "allow_sagemaker" {
-  repository = "763104351884/*"
-
-  policy = <<EOF
-{
-  "Version": "2008-10-17",
-  "Statement": [
-    {
-      "Sid": "AllowSageMakerPull",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "sagemaker.amazonaws.com"
-      },
-      "Action": [
-        "ecr:GetDownloadUrlForLayer",
-        "ecr:BatchGetImage",
-        "ecr:BatchCheckLayerAvailability"
-      ]
-    }
-  ]
-}
-EOF
-}
-
-
 resource "aws_sagemaker_model" "sentiment_model" {
-  name               = "sentiment-analysis-model"
-  execution_role_arn = aws_iam_role.sagemaker_execution_role.arn
+  name               = "retention-sentiment-analysis-model"
+  execution_role_arn = "arn:aws:iam::388691194728:role/retention/retention-sagemaker-execution-role"
 
-  primary_container { 
-    image = "763104351884.dkr.ecr.us-east-1.amazonaws.com/pytorch-inference:2.0.0-cpu-py310"
+  primary_container {
+    image          = "763104351884.dkr.ecr.us-east-1.amazonaws.com/huggingface-pytorch-inference:2.6.0-transformers4.49.0-cpu-py312-ubuntu22.04"
+    model_data_url = "s3://retention-engine-bucket/models/sentiment/model.tar.gz"
 
     environment = {
-      HF_MODEL_ID = "distilbert-base-uncased-finetuned-sst-2-english"
-      HF_TASK     = "text-classification"
+      SAGEMAKER_PROGRAM = "inference.py"
     }
   }
 }
 
 
 resource "aws_sagemaker_endpoint_configuration" "sentiment_endpoint_config" {
-  name = "sentiment-endpoint-config"
+  name = "retention-sentiment-analysis-config"
 
   production_variants {
-    variant_name           = "AllTraffic"
+    variant_name           = "primary"
     model_name             = aws_sagemaker_model.sentiment_model.name
     initial_instance_count = 1
     instance_type          = "ml.t2.medium"
@@ -50,6 +25,6 @@ resource "aws_sagemaker_endpoint_configuration" "sentiment_endpoint_config" {
 }
 
 resource "aws_sagemaker_endpoint" "sentiment_endpoint" {
-  name                 = "sentiment-endpoint"
+  name                 = "retention-sentiment-analysis-endpoint"
   endpoint_config_name = aws_sagemaker_endpoint_configuration.sentiment_endpoint_config.name
 }
