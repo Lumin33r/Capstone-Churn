dcda38324f173a73847d1ecc8890e304f1c6f7c8
okinoleiba <okino.leiba@yahoo.com>
2026-04-13 00:13:15 +0100

    add method to get guardrail id and version to bedrock script

--- sagemaker/sentiment/sentiment_training.py
+++ sagemaker/sentiment/sentiment_training.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2676ca41706e7d1cad8f0f304a23cdda2d4b50986b423807614675d16c956ea3
-size 40261
+oid sha256:2e7134683fcdc1dd7507ab683e7585672fdcba86eaf3ac8c4323279233890554
+size 41552
